a5de6e8183893c7a0342eeb39953134c75e0ac37
Themis <midinfotn401@gmail.com>
2021-04-04 20:55:39 +0100

    T 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
 # Configure ssh config file
 
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true
-}
-
+include stdlib
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/holberton',
-  replace => true
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
