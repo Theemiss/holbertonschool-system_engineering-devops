c8984d853cd36644a0553ee3adba8dd163135538
Themis <midinfotn401@gmail.com>
2021-04-12 23:25:28 +0100

    T 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,7 +12,7 @@ file_line { 'redirect':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://bayona.me permanent;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
   require => Package['nginx'],
 }
 
