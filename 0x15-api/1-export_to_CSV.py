396674ba4c77d8e4f588679a7fac09ac90ceb759
Themis <midinfotn401@gmail.com>
2021-05-04 00:02:26 +0100

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
     req = requests.get(
         'https://jsonplaceholder.typicode.com/users/' +
         (id_c) + '/todos')
-    with open("{}.csv".format(sys.argv[1]), "w") as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+    with open("{}.csv".format(sys.argv[1]), "w") as file_c:
+        writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
         for task in req.json():
             writer.writerow([id_c, username,
                             task.get("completed"), task.get("title")])
