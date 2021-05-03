fc0003e58ef74a4b56c69615bf8b096f15cd0e15
Themis <midinfotn401@gmail.com>
2021-05-03 01:23:00 +0100

    done

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import requests
+import sys
+import csv
+"""
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress
+export data in the CSV format.
+"""
+
+
+if __name__ == '__main__':
+    id_c = sys.argv[1]
+    task_title = []
+    complete = 0
+    total_task = 0
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
+    res = requests.get(url_user).json()
+    name = res['name']
+    username = res.get("username")
+    url_task = "https://jsonplaceholder.typicode.com/todos/"
+    res_task = requests.get(url_task).json()
+    for i in res_task:
+        if i.get('userId') == int(id_c):
+            if i.get('completed') is True:
+                task_title.append(i['title'])
+                complete += 1
+            total_task += 1
+    req = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' +
+        (id_c) + '/todos')
+    with open("{}.csv".format(sys.argv[1]), "w") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in req.json():
+            writer.writerow([id_c, username,
+                            task.get("completed"), task.get("title")])
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+import sys
+import json
+"""
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress
+export data in the json format.
+"""
+
+
+if __name__ == '__main__':
+    id_c = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(id_c)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(sys.argv[1])).json()
+
+    with open("{}.json".format(id_c), "w") as user_id:
+            json.dump({id_c: [{
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': users.get('username')
+            } for task in todos]}, user_id)
