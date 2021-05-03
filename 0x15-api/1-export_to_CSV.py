4626bcf0c05eb2c25c2bf4d09b7f1d279184a764
Themis <midinfotn401@gmail.com>
2021-05-03 23:59:48 +0100

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,21 +12,9 @@ import requests
 
 if __name__ == '__main__':
     id_c = sys.argv[1]
-    task_title = []
-    complete = 0
-    total_task = 0
     url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
     res = requests.get(url_user).json()
-    name = res.get('name')
     username = res.get("username")
-    url_task = "https://jsonplaceholder.typicode.com/todos/"
-    res_task = requests.get(url_task).json()
-    for i in res_task:
-        if i.get('userId') == int(id_c):
-            if i.get('completed') is True:
-                task_title.append(i['title'])
-                complete += 1
-            total_task += 1
     req = requests.get(
         'https://jsonplaceholder.typicode.com/users/' +
         (id_c) + '/todos')
